edbe715bd64e76b6d97dbe4cca11ea83aa88dc61
jobyoung05 <jobyoung05@gmail.com>
2024-08-28 17:41:21 +0100

    Fix code.ino
    Fix overflow issue that was causing major inefficiencies and unexpected behaviour

--- code/code.ino
+++ code/code.ino
@@ -1,14 +1,14 @@
-#define EEPROM_LSB 2
-#define EEPROM_MSB 9
-#define SERIAL 10
-#define SRCLK 11
-#define RCLK 12
-#define OE 13
-#define WE A0
-#define WRITE_BUTTON A1
-#define READ_BUTTON A2
+const int EEPROM_LSB = 2;
+const int EEPROM_MSB = 9;
+const int SERIALOUT = 10;
+const int SRCLK = 11;
+const int RCLK = 12;
+const int OE = 13;
+const int WE = A0;
+const int WRITE_BUTTON = A1;
+const int READ_BUTTON = A2;
 
-#define MAX_ADDR 0x7FFF
+const int MAX_ADDR = 0x7FFF;
 
 byte data[10] = {0xF0, 0xFB, 0xFC, 0xFD, 0xFE, 0xFF, 0xF0, 0xF1, 0xF8, 0xF9};
 
@@ -18,8 +18,8 @@ bool flag = true;
 void pushAddress(int address){
   /* int is always a 16-bit value, shiftOut takes a byte
      so we use shifts to get the low and high byte individually*/
-  shiftOut(SERIAL, SRCLK, MSBFIRST, byte(address >> 8));
-  shiftOut(SERIAL, SRCLK, MSBFIRST, byte(address));
+  shiftOut(SERIALOUT, SRCLK, MSBFIRST, byte(address >> 8));
+  shiftOut(SERIALOUT, SRCLK, MSBFIRST, byte(address));
 
 
   /* 74HC595 shift registers also have a storage register
@@ -47,7 +47,7 @@ void write(int addr, byte data){
 
   // making use of AT28C256 toggle bit to detect cycle completion
   
-  delay(5);
+  delay(8);
 
 }
 
@@ -78,9 +78,8 @@ void printAll(){
   char addressCode[6];
   char hexByte[2];
 
-  for (int i = 0; i <= MAX_ADDR; i += lineSize){
+  for (long i = 0; i <= MAX_ADDR; i += lineSize){
     sprintf(addressCode, "0x%04X   ", i);
-    Serial.println(i);
     Serial.print(addressCode);
     for (int j = 0; j < lineSize; j++){
       sprintf(hexByte, "%02X  ", read(i+j));
@@ -98,7 +97,7 @@ void setup() {
   digitalWrite(WE, HIGH);
 
   // outputs
-  pinMode(SERIAL, OUTPUT);
+  pinMode(SERIALOUT, OUTPUT);
   pinMode(SRCLK, OUTPUT);
   pinMode(RCLK, OUTPUT);
   pinMode(OE, OUTPUT);
@@ -124,9 +123,9 @@ void loop() {
 
   if (digitalRead(WRITE_BUTTON) & flag){
     Serial.println("WRITING EEPROM...");
-    for (int i = 0; i <= MAX_ADDR; i++){
-      write(i, 0xEA);
-      if (i%4096 == 0){
+    for (long i = 0; i <= MAX_ADDR; i++){
+      write(i, 0x00);
+      if (i%1024 == 0){
         Serial.print("#");
       }
     }
