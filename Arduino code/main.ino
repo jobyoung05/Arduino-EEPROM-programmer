203c22c2a40c8930bc21e4a5407286907767a80d
jobyoung05 <jobyoung05@gmail.com>
2024-08-28 21:34:31 +0100

    alter file structure

--- Arduino code/main.ino
+++ Arduino code/main.ino
@@ -1,3 +1,22 @@
+/*
+Arduino Uno based EEPROM programmer
+Copyright (C) 2024 Job Young
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
 const int EEPROM_LSB = 2;
 const int EEPROM_MSB = 9;
 const int SERIALOUT = 10;
@@ -45,7 +64,7 @@ void write(int addr, byte data){
   delayMicroseconds(1);
   digitalWrite(WE, HIGH);
 
-  // making use of AT28C256 toggle bit to detect cycle completion
+  //TODO: use toggle bit to detect cycle completion
   
   delay(8);
 
