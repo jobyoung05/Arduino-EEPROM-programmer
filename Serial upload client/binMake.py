60f3b008cf0580048ae66e876d4a9a369e05e01c
jobyoung05 <jobyoung05@gmail.com>
2024-09-05 16:23:41 +0100

    add license

--- Serial upload client/binMake.py
+++ Serial upload client/binMake.py
@@ -1,8 +1,30 @@
+"""
+Binary file generator for use with programmer.py
+Copyright (C) 2024 Job Young
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+
 FILE_SIZE_BYTES = 32768
 FILE_NAME = "code.bin"
 
 
 def main():
+    """ Generate a binary file of repeated bytes (0xEA) for validation
+        and testing of programmer.py
+    """
     data = bytearray()
     for i in range(FILE_SIZE_BYTES):
         data.append(0xEA)
